faf33e8b93c1f1c2dfa76655d77d350ea01f3bf7
Sivakumar Balasubramanian <siva82kb@gmail.com>
2025-06-12 12:32:13 +0530

    Added files for individual chapters.

---
+++ cprograms/helloworld.c
@@ -0,0 +1,15 @@
+/*
+ * Mechatronics for Rehabilitation Engineering.
+ * Sample code.
+ * A simple "Hello, World!" program in C.
+ * 
+ * Author: Sivakumar Balasubramanian
+ * Date: 07 June 2025
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0; // Return 0 to indicate successful execution
+}
